7dd07e095c2c015d9d7ba246bba72c9c63d2ad3e
AyushG <legendsteamprivate@gmail.com>
2025-02-05 16:55:33 +0530

    Welcome to my shit

--- CC4.c
+++ CC4.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 #define MAX_SYMBOLS 100
 #define MAX_NAME_LEN 32
@@ -10,10 +11,11 @@ typedef struct {
     int address;
 } Symbol;
 
-Symbol symbol_table[MAX_SYMBOLS]; // Changed to store Symbol structs
+Symbol symbol_table[MAX_SYMBOLS];
 int symbol_count = 0;
+int address_counter = 0;
 
-void add_symbol(const char *name, const char *type, int address) {
+void add_symbol(const char *name, const char *type) {
     if (symbol_count >= MAX_SYMBOLS) {
         fprintf(stderr, "Symbol table overflow!\n");
         return;
@@ -24,7 +26,7 @@ void add_symbol(const char *name, const char *type, int address) {
     symbol.name[MAX_NAME_LEN - 1] = '\0'; // Ensure null-termination
     strncpy(symbol.type, type, 15);
     symbol.type[15] = '\0'; // Ensure null-termination
-    symbol.address = address;
+    symbol.address = address_counter++;
 
     symbol_table[symbol_count++] = symbol;
 }
@@ -37,9 +39,52 @@ void print_symbol_table() {
     }
 }
 
+void analyze_code(const char *code) {
+    char token[MAX_NAME_LEN];
+    int i = 0, j = 0;
+    char type[16];
+
+    while (code[i] != '\0') {
+        if (isspace(code[i])) {
+            i++;
+            continue;
+        }
+
+        if (isalpha(code[i]) || code[i] == '_') {
+            j = 0;
+            while (isalnum(code[i]) || code[i] == '_') {
+                token[j++] = code[i++];
+            }
+            token[j] = '\0';
+
+            if (strcmp(token, "int") == 0 || strcmp(token, "float") == 0 || strcmp(token, "char") == 0 || strcmp(token, "long") == 0) {
+                strcpy(type, token);
+                while (isspace(code[i])) i++;
+                while (code[i] != ';' && code[i] != '\0') {
+                    j = 0;
+                    while (isalnum(code[i]) || code[i] == '_') {
+                        token[j++] = code[i++];
+                    }
+                    token[j] = '\0';
+                    if (j > 0) {
+                        add_symbol(token, type);
+                    }
+                    while (isspace(code[i])) i++;
+                    if (code[i] == ',') i++;
+                    while (isspace(code[i])) i++;
+                }
+            }
+        } else {
+            i++;
+        }
+    }
+}
+
 int main() {
-    add_symbol("x", "int", 100);
-    add_symbol("y", "float", 200);
+    char code[1000];
+    printf("Enter the C program: ");
+    fgets(code, sizeof(code), stdin);
+    analyze_code(code);
     print_symbol_table();
     return 0;
 }
